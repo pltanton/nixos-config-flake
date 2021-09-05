cb8d8f244fc4da298e80914c61d9c91cbe4d30f5
Anton Plotnikov <plotnikovanton@gmail.com>
2021-09-05 15:15:08 +0300

    Mv common desktop to thinkpad x1

--- machines/thinkpad-x1/configuration.nix
+++ machines/thinkpad-x1/configuration.nix
@@ -3,8 +3,6 @@
 {
   imports = (builtins.map (name: ./modules + "/${name}")
     (builtins.attrNames (builtins.readDir ./modules)))
-    ++ (builtins.map (name: ../../common-desktop + "/${name}")
-      (builtins.attrNames (builtins.readDir ../../common-desktop)))
     ++ (builtins.map (name: ../../common-machines + "/${name}")
       (builtins.attrNames (builtins.readDir ../../common-machines)));
 
