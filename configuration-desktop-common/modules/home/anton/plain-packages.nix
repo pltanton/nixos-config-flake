e0d8c7bf5585a7d05f1d1d545a27dceb58782ec7
Anton Plotnikov <antplotnikov@ozon.ru>
2021-10-27 12:05:22 +0300

    Delete redundant plain packages

--- configuration-desktop-common/modules/home/anton/plain-packages.nix
+++ configuration-desktop-common/modules/home/anton/plain-packages.nix
@@ -5,151 +5,153 @@ let
     sha256 = "04skv5nhbqzybxsqp7b7l4s7nl66p8cd92f6czlivwcjnj36lzya";
   }) { pkgs = pkgs; };
 in {
-  home.packages = with pkgs; lib.mkIf true [
-    hack-font
+  home.packages = with pkgs;
+    lib.mkIf true [
+      hack-font
 
-    gnuplot
+      gnuplot
 
-    nixfmt
-    shfmt
+      nixfmt
+      shfmt
 
-    gnupg
+      gnupg
 
-    ###################
-    # Packages for DE #
-    ###################
-    wdisplays
-    wf-recorder
+      ###################
+      # Packages for DE #
+      ###################
+      wdisplays
+      wf-recorder
 
-    ffmpegthumbnailer
-    pamixer
-    paprefs
-    pavucontrol
-    shared_mime_info
-    pantheon.elementary-files
+      ffmpegthumbnailer
+      pamixer
+      paprefs
+      pavucontrol
+      shared_mime_info
+      pantheon.elementary-files
+      pcmanfm
 
-    # Fonts
-    font-awesome_5
-    emojione
-    iosevka-bin
-    inter
-    #nerdfonts
+      # Fonts
+      font-awesome_5
+      emojione
+      iosevka-bin
+      inter
+      #nerdfonts
 
-    #############
-    # User apps #
-    #############
-    # CLI utils
-    nix-prefetch-github
-    gitAndTools.gh
-    libnotify
+      #############
+      # User apps #
+      #############
+      # CLI utils
+      nix-prefetch-github
+      gitAndTools.gh
+      libnotify
 
-    ffmpeg
-    # httpie
-    curlie # replacement for httpie
-    bat # cat but with highlights
-    exa # modern ls
-    duf # beauty df
-    tldr # cheatsheets for cli
-    procs # modern ps
-    ansible
-    dnsutils
-    jq
-    bitwarden-cli
-    htop
-    inetutils
-    nfs-utils
+      ffmpeg
+      # httpie
+      curlie # replacement for httpie
+      bat # cat but with highlights
+      exa # modern ls
+      duf # beauty df
+      tldr # cheatsheets for cli
+      procs # modern ps
+      ansible
+      dnsutils
+      jq
+      bitwarden-cli
+      htop
+      inetutils
+      nfs-utils
 
-    ranger
-    tmux
-    unzip
-    unrar
-    wget
-    tree
+      ranger
+      tmux
+      unzip
+      unrar
+      wget
+      tree
 
-    plantuml
+      plantuml
 
-    # GUI
-    libreoffice
+      # GUI
+      libreoffice
 
-    master.easyeffects
+      master.easyeffects
 
-    evince
-    zathura
-    gthumb
-    gnome-photos
-    gimp
-    mpv
-    master.tdesktop
-    vlc
-    xsane
-    slack
-    spotify
-    teams
+      evince
+      zathura
+      gthumb
+      gnome-photos
+      gimp
+      mpv
+      master.tdesktop
+      vlc
+      xsane
+      slack
+      spotify
+      teams
 
-    thunderbird
-    bitwarden
-    sidequest
-    scrcpy
-    darktable
-    # tigervnc
+      thunderbird
+      bitwarden
+      sidequest
+      scrcpy
+      darktable
+      # tigervnc
 
-    # Web
-    chromium
-    # nur.repos.sikmir.qutebrowser-bin
-    # qutebrowser
+      # Web
+      chromium
+      # nur.repos.sikmir.qutebrowser-bin
+      # qutebrowser
 
-    # LaTeX
-    texlive.combined.scheme-full
-    texlab
-    # (texlive.combine { inherit (texlive) scheme-medium titlesec wrapfig; })
+      # LaTeX
+      texlive.combined.scheme-full
+      texlab
+      # (texlive.combine { inherit (texlive) scheme-medium titlesec wrapfig; })
 
-    # Dev
-    docker-compose
-    clang-tools
-    kubectl
-    kubectx
-    sqlite
-    postgresql
-    coreutils
-    # gradle2nix
-    grpc
-    evans # grpc client
-    graphviz
-    nodejs
-    insomnia
-    postman
-    # jetbrains.idea-ultimate
-    jetbrains.idea-community
-    # jetbrains.datagrip
-    # android-studio
-    mongodb
-    robo3t
-    dbeaver
-    gcc
-    black # Python code formatter
-    python-language-server
-    python38Packages.pip
-    (python3.withPackages (pp:
-      with pp; [
-        pip
-        pylint
-        pyright
-        jedi
-        flake8
-        autopep8
-        pygments
-        hvac
-        pika
-      ]))
+      # Dev
+      docker-compose
+      clang-tools
+      kubectl
+      kubectx
+      sqlite
+      postgresql
+      coreutils
+      # gradle2nix
+      grpc
+      evans # grpc client
+      graphviz
+      nodejs
+      insomnia
+      postman
+      # jetbrains.idea-ultimate
+      jetbrains.idea-community
+      # jetbrains.datagrip
+      # android-studio
+      mongodb
+      robo3t
+      dbeaver
+      gcc
+      black # Python code formatter
+      python-language-server
+      python38Packages.pip
+      (python3.withPackages (pp:
+        with pp; [
+          pip
+          pylint
+          pyright
+          jedi
+          flake8
+          autopep8
+          pygments
+          hvac
+          pika
+        ]))
 
-    nodePackages.yaml-language-server
-    nodePackages.prettier
-    nodePackages.vue-language-server
-    nodePackages.eslint
-    gnumake
-    zip
+      nodePackages.yaml-language-server
+      nodePackages.prettier
+      nodePackages.vue-language-server
+      nodePackages.eslint
+      gnumake
+      zip
 
-    # stable.awscli2
-    # stable.aws-sam-cli
-  ];
+      # stable.awscli2
+      # stable.aws-sam-cli
+    ];
 }
