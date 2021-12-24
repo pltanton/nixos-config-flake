84faf83a1ab60dda9cfb11c433965b387405579f
Anton Plotnikov <antplotnikov@ozon.ru>
2021-12-24 18:04:33 +0300

    Snapshot

--- configuration-desktop-common/modules/networking.nix
+++ configuration-desktop-common/modules/networking.nix
@@ -3,6 +3,8 @@
     nameservers = [ "10.100.0.1" ];
     hosts = { };
     networkmanager.enable = true;
+    resolvconf.extraOptions = [ "rotate" "timeout:1" ];
+    search = [ "thinkpad-x1.home" ];
 
     firewall.allowedUDPPorts = [ 8080 ];
     firewall.allowedTCPPorts = [ 8080 ];
