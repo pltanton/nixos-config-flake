64469561334ccdde7cd58457560bde61c88a0608
Anton Plotnikov <antplotnikov@ozon.ru>
2022-03-23 17:47:00 +0300

    Update git config

--- configuration-per-host/thinkbook/modules/home/anton/git.nix
+++ configuration-per-host/thinkbook/modules/home/anton/git.nix
@@ -3,8 +3,8 @@
     userEmail = "antplotnikov@ozon.ru";
 
     signing = {
-     key = "C5E9ED0B49F6532A3551DF0585ABC45A273326BD";
-     signByDefault = true;
+      key = "E49A7BC8572859AB9609F6E9311C09268E05E376";
+      signByDefault = true;
     };
   };
 }
