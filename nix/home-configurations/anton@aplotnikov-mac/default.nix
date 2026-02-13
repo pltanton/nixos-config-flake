3d24be60843ed2ec8b5fca586c90c667f9bbf335
Anton Plotnikov <plotnikovanton@gmail.com>
2026-02-13 10:03:25 +0200

    Snapshot

--- nix/home-configurations/anton@aplotnikov-mac/default.nix
+++ nix/home-configurations/anton@aplotnikov-mac/default.nix
@@ -5,6 +5,13 @@
     inputs.self.homeModules.anton
     inputs.self.homeModules.anton-mac
 
+    ({config, ...}: {
+      sops = {
+        defaultSopsFile = ../../nixos-modules/sops/anton.yaml;
+        age.keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+      };
+    })
+
     {
       home.stateVersion = "25.05";
       home.homeDirectory = "/Users/anton";
